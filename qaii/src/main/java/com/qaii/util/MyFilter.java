299c538b8f222c6d99b6a22deb07b1ed8722540e
hkpmbylove <1115181693@qq.com>
2018-12-24 10:31:32 +0800

    拦截图片过滤器

---
+++ qaii/src/main/java/com/qaii/util/MyFilter.java
@@ -0,0 +1,42 @@
+package com.qaii.util;
+/**
+ * Created by kunpeng on 2018/12/21 16:33
+ */
+import sun.java2d.loops.DrawGlyphListAA;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class MyFilter implements Filter{
+
+    private FilterConfig filterConfig = null;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;	//request对象
+        HttpServletResponse response = (HttpServletResponse) res;//response对象
+
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragrma", "no-cache");
+        response.setDateHeader("Expires", 0);//禁止浏览器缓存
+        String referer = request.getHeader("referer");    //链接的来源地址
+        String url=request.getRequestURI();
+        String serverName=request.getServerName();
+        System.out.println(serverName);
+        System.out.println(url);
+        if (url.contains("/QAII/image")||referer.contains("http://localhost")) {//判断访问来源
+            chain.doFilter(request, response); //正常显示图片
+        } else {
+            request.getRequestDispatcher("index.html").forward(request, response);//显示错误图片
+        }
+    }
+
+    public void destroy(){
+        filterConfig = null;
+    }
+}
